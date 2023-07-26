e06eb84679c230f4874782f23299ded05dbc985d
Subhayan0022 <msubhayan529@gmail.com>
2023-07-26 21:34:16 +0530

    achievement done

--- src/components/infocard.js
+++ src/components/infocard.js
@@ -18,7 +18,7 @@ function InfoCard(props) {
 
     return ( 
         <div class="flex flex-col portfolio-card m-auto rounded-3xl h-full w-11/12">
-            <div class="flex justify-center m-auto h-1/6 w-full shadow-xl rounded-xl">
+            <div class="flex justify-center m-auto shrink-0 h-40 w-full shadow-xl rounded-xl">
                 <button onClick={()=>{
                     updateCurrentButton("SkillsCard")
                 }} class="info-button rounded-xl px-8 m-auto">Skills</button>
@@ -29,7 +29,7 @@ function InfoCard(props) {
                     updateCurrentButton("AchievementsCard")
                 }} class="info-button rounded-xl px-8 m-auto">Achievements</button>
             </div>
-            <div class="flex h-full w-full">
+            <div class="flex h-full w-full overflow-y-scroll">
                 {infoCardButton[currentButton]}
             </div>
         </div>
--- src/components/infocardcomp/achievements.js
+++ src/components/infocardcomp/achievements.js
@@ -1,9 +1,27 @@
 import cert1 from "../../assets/achiv1.jpeg"
+import cert2 from "../../assets/achiv2.jpg"
 
 function Achievements(props) {
     return ( 
-        <div class="flex h-5/6 w-full m-auto">
-            
+        <div class="flex flex-col h-full w-full m-auto overflow-y-scroll">
+            <div class="flex h-full w-full shrink-0">
+                <div class="flex achiv-card h-5/6 w-9/12 m-auto">
+                    <div class="flex m-auto h-full w-10/12">
+                        <div class="flex h-5/6 w-11/12 m-auto rounded-xl">
+                            <img class="hover:scale-125 duration-500 h-full w-full rounded-lg" src={cert1}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex h-full w-full shrink-0">
+                <div class="flex achiv-card h-5/6 w-9/12 m-auto">
+                    <div class="flex m-auto h-full w-10/12">
+                        <div class="flex h-5/6 w-11/12 m-auto rounded-xl">
+                            <img class="hover:scale-125 duration-500 h-full w-full rounded-lg" src={cert2}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
      );
 }

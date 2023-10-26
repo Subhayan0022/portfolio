ca5daaab8a454e2b678f0e7cdbb5c6031822db8e
Subhayan0022 <msubhayan529@gmail.com>
2023-10-26 18:15:11 +0530

    some visual changes

--- src/components/introcard.js
+++ src/components/introcard.js
@@ -1,4 +1,4 @@
-import pfp from "../assets/face.png"
+import pfp from "../assets/face.gif"
 import arrow from "../assets/arrow.png"
 import resumepdf from "../assets/Resume.pdf"
 
@@ -6,17 +6,17 @@ function IntroCard(props) {
     return ( 
         <div class="flex flex-col h-full w-full">
             <div class="flex flex-row h-4/6 w-full">
-                <div class="flex m-auto h-4/6 w-8/12">
-                    <div class="w-5/12 m-auto">
-                        <img id="code" src={pfp} alt="..." class="h-full w-full animate-pulse" />
+                <div class="flex ml-10 m-auto h-4/6 w-8/12">
+                    <div class="h-full w-full m-auto">
+                        <img id="code" src={pfp} alt="..." class="h-full w-full "/>
                     </div>
                 </div>
                 <div class="flex flex-col  portfolio-card m-auto h-4/6 w-full">
                     
                     <div class="flex typewriter flex-col overflow-auto h-full w-10/12 mt-10">
-                        <h1 class="portfolio-title text-center text-6xl">HI! I'M SUBHAYAN ;</h1>
+                        <h1 class="portfolio-title text-center text-6xl">&lt;HI! I'M SUBHAYAN./&gt; </h1>
                         <h2 class="portfolio-text text-2xl p-2 mt-10 text-center ">I'm a Student Developer currently pursuing my undergrad degree in Bachelor of Computer Applications (BCA).</h2>
-                        <h3 class="portfolio-text text-2xl p-2 text-center">I also develop Games and Webistes while actively learning about the newest technological buzz..</h3>
+                        <h3 class="portfolio-text text-2xl p-2 text-center">I develop Games on Unreal Engine and also create web apps while actively learning about the newest technological buzz.</h3>
                         <a class="m-auto" href={resumepdf} download="Resume-PDF-document" target="_blank" rel="noreferrer">
                             <button class="project-button rounded-xl px-8 ">RESUME</button>
                         </a>

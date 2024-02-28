877b0e292434194b69f401951af8b38598514061
Subhayan0022 <msubhayan529@gmail.com>
2024-02-28 17:03:43 +0530

    new Projects added and minor changes made

--- src/components/infocardcomp/projects.js
+++ src/components/infocardcomp/projects.js
@@ -1,7 +1,9 @@
 import bxs from "../../assets/bxs.jpg"
 import ballgame from "../../assets/bgame.png"
 import portfolio from "../../assets/port.png"
-import certgen from "../../assets/certgen.png";
+import certgen from "../../assets/certgen.png"
+import dino from "../../assets/dino.png"
+import upi from "../../assets/upi.png"
 
 import { useEffect } from "react"
 
@@ -24,6 +26,7 @@ function Projects(props) {
           observer.observe(document.querySelectorAll('.project-card')[1]);
           observer.observe(document.querySelectorAll('.project-card')[2]);
           observer.observe(document.querySelectorAll('.project-card')[3]);
+          observer.observe(document.querySelectorAll('.project-card')[4]);
 
     })
 
@@ -61,7 +64,7 @@ function Projects(props) {
                     </div>
                 </div>
             </div>
-            <div class="flex h-3/6 w-full shrink-0 ">
+            {/* <div class="flex h-3/6 w-full shrink-0 ">
                 <div class="flex flex-row project-card h-5/6 w-9/12 m-auto">
                     <div class="flex h-full w-10/12">
                         <div class="flex h-5/6 w-11/12 m-auto rounded-xl">
@@ -86,7 +89,7 @@ function Projects(props) {
                         </div>
                     </div>
                 </div>                      
-            </div>
+            </div> */}
             <div class="flex h-3/6 w-full shrink-0">
                 <div class="flex flex-row project-card h-5/6 w-9/12 m-auto">
                     <div class="flex h-full w-10/12">
@@ -151,6 +154,71 @@ function Projects(props) {
                     </div>
                 </div>
             </div>
+            <div class="flex h-3/6 w-full shrink-0">
+                <div class="flex flex-row project-card h-5/6 w-9/12 m-auto">
+                    <div class="flex h-full w-10/12">
+                        <div class="flex h-5/6 w-11/12 m-auto rounded-xl">
+                            <img class="hover:scale-125 duration-500 h-full m-auto w-full rounded-lg" src={upi}/>
+                        </div>
+                    </div>
+                    <div class="flex flex-col h-full w-full">
+                        
+                        <div class="flex flex-col h-full w-full">
+                            <div class="flex h-4/6 w-full">
+                                <h1 class="portfolio-title text-5xl p-5 mt-5">Uniplacer Insight</h1>
+                            </div>
+                            <div class="flex h-4/6 w-full">
+                                <h1 class="portfolio-text p-5 text-2xl">A Software Application used by University Placement Cell for visual insights and Representation of Student and Company Data.</h1>
+                            </div>
+                        </div>
+                        <div class="flex flex-row h-3/6 w-full">
+                            {/* <div class="flex h-full w-full">
+                                <a class="m-auto" href="https://srmcshktr.com/certificate-generator/" target="_blank" rel="noopener noreferrer">
+                                    <button class="project-button rounded-xl px-8 m-auto">Go to site</button>
+                                </a>
+                            </div> */}
+                            <div class="flex h-full w-full">
+                                <a class="m-auto" href="https://github.com/Subhayan0022/certificate-generator/" target="_blank" rel="noopener noreferrer">
+                                    <button class="project-button rounded-xl px-8">Go to code</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex h-3/6 w-full shrink-0">
+                <div class="flex flex-row project-card h-5/6 w-9/12 m-auto">
+                    <div class="flex h-full w-10/12">
+                        <div class="flex h-5/6 w-11/12 m-auto rounded-xl">
+                            <img class="hover:scale-125 duration-500 h-full m-auto w-full rounded-lg" src={dino}/>
+                        </div>
+                    </div>
+                    <div class="flex flex-col h-full w-full">
+                        
+                        <div class="flex flex-col h-full w-full">
+                            <div class="flex h-4/6 w-full">
+                                <h1 class="portfolio-title text-5xl p-5 mt-5">Dino Game Clone in Python</h1>
+                            </div>
+                            <div class="flex h-4/6 w-full">
+                                <h1 class="portfolio-text p-5 text-2xl">The famous Google Dino Game made in Python to learn basics of PyGame.</h1>
+                            </div>
+                        </div>
+                        <div class="flex flex-row h-3/6 w-full">
+                            {/* <div class="flex h-full w-full">
+                                <a class="m-auto" href="https://srmcshktr.com/certificate-generator/" target="_blank" rel="noopener noreferrer">
+                                    <button class="project-button rounded-xl px-8 m-auto">Go to site</button>
+                                </a>
+                            </div> */}
+                            <div class="flex h-full w-full">
+                                <a class="m-auto" href="https://github.com/Subhayan0022/certificate-generator/" target="_blank" rel="noopener noreferrer">
+                                    <button class="project-button rounded-xl px-8">Go to code</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
         </div>
      );
 }
